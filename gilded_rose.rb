6dfb7abcafccf395e6e13202d98c872a8995066f
Cristian Benitez <clbenitez64@gmail.com>
2017-03-16 20:38:31 -0500

    refactor final

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,103 +1,75 @@
-VALUES_TO_QUALITY_LOWER_THAN_ZERO = {true => {true => -2, false => -1}, false => {true => 0, false => 0}}
-VALUES_TO_A = {true => {true => 2, false => 1}, false => {true => 0, false => 0}}
-VALUES_TO_S= {true => 1, false => 0}
-
 def update_quality(items)
   items.each do |item|
-    item.quality = item.quality + send("return_quality_value_to_#{item.name[0]}".to_sym) 
-    item.sell_in -= 1
-    if item.name == 'Sulfuras, Hand of Ragnaros'
-      item.sell_in += 1
-    end
-    item.save
+    rest_one_quality_when_no_aged_no_sulfuras_no_backstage_and_quality_upper_than_zero(item)
+    sum_one_quality_when_name_and_quality_lower_than_fifty(item, "Aged")
+    sum_one_quality_when_name_and_quality_lower_than_fifty(item, "Sulfuras")
+    sum_one_quality_when_name_and_quality_lower_than_fifty(item, "Backstage")
+    sum_one_quality_when_backstage_and_quality_lower_than_fifty_and_sell_in_lower_than_number(item, 11)
+    sum_one_quality_when_backstage_and_quality_lower_than_fifty_and_sell_in_lower_than_number(item, 6)
+    rest_one_sell_in(item) 
+    sum_one_quality_when_aged_sell_lower_than_zero_and_quality_lower_than_fifty(item)
+    quality_eql_to_zero(item)
+    rest_one_quality_when_no_aged_no_sulfuras_and_sell_lower_than_zero_and_quality_upper_than_zero(item)
   end
 end
 
-def return_quality_value_to_5(item)
-  VALUES_TO_QUALITY_LOWER_THAN_ZERO[quality_upper_than_zero(item)][sell_in_lower_than_zero(item)]
-end
+private
 
-def return_quality_value_to_E(item)
-  VALUES_TO_QUALITY_LOWER_THAN_ZERO[quality_upper_than_zero(item)][sell_in_lower_than_zero(item)]
-end
-
-def return_quality_value_to_C(item)
-  VALUES_TO_QUALITY_LOWER_THAN_ZERO[quality_upper_than_zero(item)][sell_in_lower_than_zero(item)]
-end
-
-def return_quality_value_to_A(item)
-  VALUES_TO_A[quality_lower_than_fifty(item)][sell_in_lower_than_zero(item)]
-end
-
-def return_quality_value_to_S(item)
-  VALUES_TO_S[quality_lower_than_fifty(item)]
-end
-
-def return_quality_value_to_N(item)
-  VALUES_TO_A[quality_lower_than_fifty(item)][sell_in_lower_than_zero(item)]
-end
-
-def return_quality_value_to_B(item)
-  x = 0
-  if item.quality < 50
-    x =+1
-    if item.sell_in < 11
-      x =+1
-    end
-    if item.sell_in < 6
-      x =+1
-    end
+  def sum_one_quality_when_backstage_and_quality_lower_than_fifty_and_sell_in_lower_than_number(item, number)
+    sum_one_quality(item) if quality_lower_than_fifty(item) && name_include?(item,'Backstage') && sell_in_lower_than(item, number)
   end
-  if item.sell_in < 0 && item.quality > 0
-    x =-1
+
+  def sum_one_quality_when_name_and_quality_lower_than_fifty(item, name)
+    sum_one_quality(item) if quality_lower_than_fifty(item) && name_include?(item,name)
   end
-end
 
-
-
-
-
-def quality_upper_than_zero(item)
-  item.quality > 0
-end
-
-def quality_lower_than_fifty(item)
-  item.quality < 50
-end
-
-def sell_in_lower_than_zero(item)
-  item.sell_in < 0
-end
-
-def sell_in_lower_than_six(item)
-  item.sell_in < 6
-end
-
-def sell_in_lower_than_eleven(item)
-  item.sell_in < 11
-end
-
-
-
-def sum_one_quality(item)
-  item.quality += 1
-end
-
-def rest_one_quality(item)
-  item.quality -= 1
-end
-
-def quality_eql_to_zero(item)
-  if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 0
-    item.quality = 0
+  def rest_one_quality_when_no_aged_no_sulfuras_no_backstage_and_quality_upper_than_zero(item)
+    rest_one_quality(item) if !name_include?(item,'Aged') && !name_include?(item,'Backstage') && !name_include?(item,'Sulfuras') && quality_upper_than_zero?(item)
   end
-end
 
-def rest_one_sell_in(item)
-  if item.name != 'Sulfuras, Hand of Ragnaros'
-    item.sell_in -= 1
+  def rest_one_quality_when_no_aged_no_sulfuras_and_sell_lower_than_zero_and_quality_upper_than_zero(item)
+    rest_one_quality(item) if !name_include?(item,"Aged") && !name_include?(item,"Sulfuras") && sell_in_lower_than_zero?(item) && quality_upper_than_zero?(item)
   end
-end
+
+  def sum_one_quality_when_aged_sell_lower_than_zero_and_quality_lower_than_fifty(item)
+    sum_one_quality(item) if name_include?(item,"Aged") && sell_in_lower_than_zero?(item) && quality_lower_than_fifty(item)
+  end
+
+  def name_include?(item, name)
+    item.name.include?(name)
+  end
+
+  def sell_in_lower_than(item, number)
+    item.sell_in < number
+  end
+
+  def quality_lower_than_fifty(item)
+    item.quality < 50
+  end
+
+  def sell_in_lower_than_zero?(item)
+    item.sell_in < 0
+  end
+
+  def quality_upper_than_zero?(item)
+    item.quality > 0
+  end
+
+  def sum_one_quality(item)
+    item.quality += 1
+  end
+
+  def rest_one_quality(item)
+    item.quality -= 1
+  end
+
+  def quality_eql_to_zero(item)
+    item.quality = 0 if name_include?(item, 'Backstage') && sell_in_lower_than_zero?(item)
+  end
+
+  def rest_one_sell_in(item)
+    item.sell_in -= 1 if !name_include?(item,"Sulfuras")
+  end
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
